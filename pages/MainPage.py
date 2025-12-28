bf9e186e82e7b0a298f6a1af03e8cc0e4bd49ea5
Dur1lka <polinastex15@yandex.ru>
2025-12-28 21:37:12 +0300

    Revert "доработки"
    This reverts commit 179a3e2df90126f58443972192ab97de6b5fe74c.

--- pages/MainPage.py
+++ pages/MainPage.py
@@ -6,21 +6,17 @@ from selenium.webdriver.support import expected_conditions as EC
 class MainPage:
     def __init__(self,driver:WebDriver):
         self.__driver=driver
-        self.wait=WebDriverWait(driver,10)
     # Получаем текущий URL
     def get_current_url(self,param:str):
         return self.__driver.current_url
     # Нажимаем на иконку с именем в верхней части экрана
     def open_menu(self):
-        self.__driver.find_element(By.CSS_SELECTOR,"button[aria-label='Меню профиля']").click()
-    # Получаем информацию о пользователе(проверить надпись "войти")
-    # def get_account_info(self,param:list[str]):
-    #     # Ищем имя пользователя:
-    #     container=self.__driver.find_element(By.CSS_SELECTOR,"div[profile-page_user-name]")
-    #     fields=container.find_element(By.CSS_SELECTOR,"div")
-    #     name=fields[0].text
-    # # Возвращаем имя пользователя:
-    # return [name]
-    def get_register_modal_title(self):
-        modal_title=self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,".ui-header-modal__title")))
-        return modal_title.text
+        self.__driver.find_element(By.CSS_SELECTOR,"button[aria-label='меню профиля']").click()
+    # Получаем информацию о пользователе:
+    def get_account_info(self,param:list[str]):
+        # Ищем имя пользователя:
+        container=self.__driver.find_element(By.CSS_SELECTOR,"div[profile-page_user-name]")
+        fields=container.find_element(By.CSS_SELECTOR,"div")
+        name=fields[0].text
+    # Возвращаем имя пользователя:
+    return [name]
